51b171ff425000a099a7d32a0157c4e8315daf8f
triennc0209 <harryson1134@gmail.com>
2025-12-05 17:24:17 +0700

    feat(incoming-doc-view): create Incoming Doc View

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentHub.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<PinCode> PinCodes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=app.db");
+    }
+
+    public class PinCode
+    {
+        public int Id { get; set; }
+        public required string Code { get; set; }
+    }
+}
