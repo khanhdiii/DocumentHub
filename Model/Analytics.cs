fb6925222d9decc09f3ab12442ab2efaf9eb6e03
khanhdiii <khanh12369874@gmail.com>
2025-12-25 08:25:14 +0700

    Feat/Analytics: Create UI Chart

---
+++ Model/Analytics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentHub.Model
+{
+    public class Analytics
+    {
+        public int Month { get; set; }
+        public int IncomingDocument { get; set; }
+        public int OutgoingDocument { get; set; }
+    }
+}
---
+++ ViewModel/AnalyticsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DocumentHub.ViewModel
+{
+    public class AnalyticsViewModel
+    {
+        public SeriesCollection Series { get; set; }
+
+        public AnalyticsViewModel()
+        {
+            Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Văn bản đến",
+                    Values = new ChartValues<int> { 12, 15, 20, 18 }
+                },
+                new LineSeries
+                {
+                    Title = "Văn bản đi",
+                    Values = new ChartValues<int> { 10, 12, 18, 22 }
+                }
+            };
+        }
+    }
+}
