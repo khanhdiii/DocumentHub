8b555a80ee83b99f8bea477a4a675be1cbdd92f4
khanhdiii <khanh12369874@gmail.com>
2025-12-26 08:52:30 +0700

    feat/signer: create view for form

---
+++ Model/Signer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentHub.Model
+{
+    public class Signer : INotifyPropertyChanged 
+    { 
+        public int Id { get; set; }
+        private string _fullName; 
+        public string FullName 
+        { 
+            get => _fullName;
+            set { _fullName = value;
+                OnPropertyChanged(nameof(FullName));
+            } 
+        } 
+        private string _position; 
+        public string Position 
+        { 
+            get => _position;
+            set { _position = value;
+                OnPropertyChanged(nameof(Position));
+            } 
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
