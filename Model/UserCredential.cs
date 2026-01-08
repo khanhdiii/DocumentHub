4dcea3c9ec5a1f1b4bfd5b7ae3eb7dce7c66f76e
khanhdiii <khanh12369874@gmail.com>
2026-01-08 09:38:32 +0700

    feat/login-form: create model for auth

---
+++ Model/UserCredential.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentHub.Model
+{
+    public class UserCredential
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(6)]
+        public string PIN { get; set; } = "";
+
+        [Required]
+        public string SecurityQuestion1 { get; set; } = "";
+
+        [Required]
+        public string SecurityAnswer1 { get; set; } = "";
+
+        [Required]
+        public string SecurityQuestion2 { get; set; } = "";
+
+        [Required]
+        public string SecurityAnswer2 { get; set; } = "";
+
+        [Required]
+        public string SecondaryPassword { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastUpdated { get; set; }
+    }
+}
