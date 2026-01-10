d9d3a718a8c3c6f062695d1a06390bb92e361c8b
khanhdiii <khanh12369874@gmail.com>
2026-01-10 15:44:28 +0700

    feat/work-preogress: Create view and hanle with database

--- Model/WorkProgress.cs
+++ Model/WorkProgress.cs
@@ -1,31 +1,180 @@
-﻿using System;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 
 namespace DocumentHub.ViewModel
 {
-    public class WorkProgress
+    public class WorkProgress : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? Deadline { get; set; }
-        public DateTime? NotificationDate { get; set; }
-        public Person Assigner { get; set; }
-        public Person PersonInCharge { get; set; }
-        public string Status { get; set; }
-        public string Priority { get; set; }
-        public int Progress { get; set; }
-        public DateTime? ActualCompletionDate { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id
+        {
+            get; set;
+        }
 
-        public bool IsMonth { get; set; }
-        public bool Is3Months { get; set; }
-        public bool Is6Months { get; set; }
-        public bool Is9Months { get; set; }
-        public bool IsYear { get; set; }
-    }
+        private string _name;
+        public string Name
+        {
+            get => _name; set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? StartDate
+        {
+            get; set;
+        }
+        public DateTime? Deadline
+        {
+            get; set;
+        }
+
+        public class Person
+        {
+            public string FullName
+            {
+                get; set;
+            }
+        }
+        public Person Assigner
+        {
+            get; set;
+        }
+        public Person PersonInCharge
+        {
+            get; set;
+        }
+
+        public string Status
+        {
+            get; set;
+        }
+        public string Priority
+        {
+            get; set;
+        }
+        public int Progress
+        {
+            get; set;
+        }
+        public DateTime? ActualCompletionDate
+        {
+            get; set;
+        }
+
+        // IMPORTANT:  PropertyChanged
+        private bool _isMonth;
+        public bool IsMonth
+        {
+            get => _isMonth;
+            set
+            {
+                _isMonth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isYear;
+        public bool IsYear
+        {
+            get => _isYear;
+            set
+            {
+                _isYear = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isYearly;
+        public bool IsYearly
+        {
+            get => _isYearly;
+            set
+            {
+                _isYearly = value;
+                OnPropertyChanged();
+            }
+        }
 
 
-    public class Person
-    {
-        public string FullName { get; set; }
+        private DateTime? _notificationDate;
+        public DateTime? NotificationDate
+        {
+            get => _notificationDate;
+            set
+            {
+                _notificationDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private bool _is3Months;
+        public bool Is3Months
+        {
+            get => _is3Months; set
+            {
+                _is3Months = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _is6Months;
+        public bool Is6Months
+        {
+            get => _is6Months; set
+            {
+                _is6Months = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _is9Months;
+        public bool Is9Months
+        {
+            get => _is9Months; set
+            {
+                _is9Months = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isSudden;
+        public bool IsSudden
+        {
+            get => _isSudden; set
+            {
+                _isSudden = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isSeminar;
+        public bool IsSeminar
+        {
+            get => _isSeminar; set
+            {
+                _isSeminar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? SeminarDate
+        {
+            get; set;
+        }
+        public DateTime? SuddenDate
+        {
+            get; set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string n = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
     }
 }
--- ViewModel/WorkProgressViewModel.cs
+++ ViewModel/WorkProgressViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
@@ -12,28 +12,112 @@ namespace DocumentHub.ViewModel
         public ObservableCollection<WorkProgress> WorkList { get; set; } = new();
         public ObservableCollection<string> StatusOptions { get; set; } = new() { "Chưa bắt đầu", "Đang thực hiện", "Hoàn thành", "Tạm hoãn" };
         public ObservableCollection<string> PriorityOptions { get; set; } = new() { "Thấp", "Trung bình", "Cao", "Khẩn cấp" };
-        public ObservableCollection<Person> Assigners { get; set; } = new();
-        public ObservableCollection<Person> StaffList { get; set; } = new();
+        public class Person { public string FullName { get; set; } }
+        public ObservableCollection<SelectableMonth> MonthOptions { get; } = new();
+        public ObservableCollection<SelectableYear> YearOptions { get; } = new();
+        public WorkProgressViewModel()
+        {
+            WorkItem = new WorkProgress();
+            WorkItem.PropertyChanged += WorkItem_PropertyChanged;
+        }
+
+        private void WorkItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(WorkProgress.IsMonth))
+            {
+                if (WorkItem.IsMonth)
+                    GenerateMonthOptions();
+                else
+                    MonthOptions.Clear();
+            }
+            else if (e.PropertyName == nameof(WorkProgress.IsYear) || e.PropertyName == nameof(WorkProgress.NotificationDate))
+            {
+                if (WorkItem.IsYear)
+                    GenerateYearOptions();
+                else
+                    YearOptions.Clear();
+            }
+        }
+
+        private bool _isMonthSelected;
+        public bool IsMonthSelected
+        {
+            get => _isMonthSelected;
+            set
+            {
+                _isMonthSelected = value;
+                OnPropertyChanged();
+                if (value)
+                    GenerateMonthOptions();
+                else
+                    MonthOptions.Clear();
+            }
+        }
+
+        private bool _isYearSelected;
+        public bool IsYearSelected
+        {
+            get => _isYearSelected;
+            set
+            {
+                _isYearSelected = value;
+                OnPropertyChanged();
+                if (value)
+                    GenerateYearOptions();
+                else
+                    YearOptions.Clear();
+            }
+        }
+
+        private void GenerateMonthOptions()
+        {
+            MonthOptions.Clear();
+            for (int i = 1; i <= 12; i++)
+                MonthOptions.Add(new SelectableMonth { Month = i, MonthLabel = $"Tháng {i}" });
+        }
+
+
+        private void GenerateYearOptions()
+        {
+            YearOptions.Clear();
+            int startYear = WorkItem.NotificationDate?.Year ?? DateTime.Now.Year;
+            for (int i = 0; i < 10; i++)
+            {
+                YearOptions.Add(new SelectableYear { Year = startYear + i });
+            }
+        }
 
         private WorkProgress _selectedWorkItem;
         public WorkProgress SelectedWorkItem
         {
             get => _selectedWorkItem;
-            set { _selectedWorkItem = value; OnPropertyChanged(); }
+            set
+            {
+                _selectedWorkItem = value;
+                OnPropertyChanged();
+            }
         }
 
         private string _searchKeyword;
         public string SearchKeyword
         {
             get => _searchKeyword;
-            set { _searchKeyword = value; OnPropertyChanged(); }
+            set
+            {
+                _searchKeyword = value;
+                OnPropertyChanged();
+            }
         }
 
         private string _notificationMessage;
         public string NotificationMessage
         {
             get => _notificationMessage;
-            set { _notificationMessage = value; OnPropertyChanged(); }
+            set
+            {
+                _notificationMessage = value;
+                OnPropertyChanged();
+            }
         }
 
         public WorkProgress WorkItem { get; set; } = new();
@@ -49,8 +133,7 @@ namespace DocumentHub.ViewModel
 
             CalculateNotifications(WorkItem);
 
-            WorkItem = new WorkProgress();
-            OnPropertyChanged(nameof(WorkItem));
+            CreateNewWorkItem();
         }
 
         private void DeleteWorkItem()
@@ -64,24 +147,102 @@ namespace DocumentHub.ViewModel
             if (SelectedWorkItem != null)
                 SelectedWorkItem.Status = "Hoàn thành";
         }
+        private void CreateNewWorkItem()
+        {
+            WorkItem = new WorkProgress();
+            WorkItem.PropertyChanged += WorkItem_PropertyChanged;
+            OnPropertyChanged(nameof(WorkItem));
+            UpdateOptions();
+        }
+
+        // Khi WorkItem.IsMonth thay đổi → gọi GenerateMonthOptions
+        private void UpdateOptions()
+        {
+            if (WorkItem.IsMonth)
+                GenerateMonthOptions();
+            else
+                MonthOptions.Clear();
+            if (WorkItem.IsYear)
+                GenerateYearOptions();
+            else
+                YearOptions.Clear();
+        }
+
+        public class SelectableMonth : INotifyPropertyChanged
+        {
+            public int Month
+            {
+                get; set;
+            }
+            public string MonthLabel
+            {
+                get; set;
+            }
+            private bool _isSelected;
+            public bool IsSelected
+            {
+                get => _isSelected; set
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string n = null) =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
+        }
+
+        public class SelectableYear : INotifyPropertyChanged
+        {
+            public int Year { get; set; }
+            private bool _isYear;
+            public bool IsYear { get => _isYear; set { _isYear = value; OnPropertyChanged(); } }
+            private bool _isSelected;
+            public bool IsSelected
+            {
+                get => _isSelected; set
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string n = null) =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
+        }
+
+
 
         private void CalculateNotifications(WorkProgress item)
         {
-            if (item.NotificationDate == null) return;
-
+            if (item.NotificationDate == null)
+                return;
             DateTime baseDate = item.NotificationDate.Value;
             List<string> messages = new();
 
-            if (item.IsMonth)
-                messages.Add($"Thông báo sau 1 tháng: {baseDate.AddMonths(1):dd/MM/yyyy}");
+            foreach (var m in MonthOptions.Where(x => x.IsSelected))
+                messages.Add($"Thông báo sau {m.Month} tháng: {baseDate.AddMonths(m.Month):dd/MM/yyyy}");
+
+            foreach (var y in YearOptions.Where(x => x.IsSelected))
+            {
+                var target = new DateTime(y.Year, baseDate.Month, Math.Min(baseDate.Day, DateTime.DaysInMonth(y.Year, baseDate.Month)));
+                messages.Add($"Thông báo năm {y.Year}: {target:dd/MM/yyyy}");
+            }
+
             if (item.Is3Months)
                 messages.Add($"Thông báo sau 3 tháng: {baseDate.AddMonths(3):dd/MM/yyyy}");
             if (item.Is6Months)
                 messages.Add($"Thông báo sau 6 tháng: {baseDate.AddMonths(6):dd/MM/yyyy}");
             if (item.Is9Months)
                 messages.Add($"Thông báo sau 9 tháng: {baseDate.AddMonths(9):dd/MM/yyyy}");
-            if (item.IsYear)
-                messages.Add($"Thông báo sau 1 năm: {baseDate.AddYears(1):dd/MM/yyyy}");
+            if (item.IsYearly)
+                messages.Add($"Nhắc hàng năm vào {baseDate:dd/MM}");
+
+            if (item.IsSudden && item.SuddenDate != null)
+                messages.Add($"Thông báo đột xuất vào ngày: {item.SuddenDate:dd/MM/yyyy}");
+
+            if (item.IsSeminar && item.SeminarDate != null)
+                messages.Add($"Thông báo chuyên đề: {item.SeminarDate:dd/MM/yyyy}");
 
             NotificationMessage = string.Join(" | ", messages);
         }
