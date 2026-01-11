dbcfa007a063d7ca6c0696779adf2c16b9d21ee8
khanhdiii <khanh12369874@gmail.com>
2026-01-11 14:32:47 +0700

    feat/work-progress: create model for list checkbox months and years

---
+++ Model/WorkProgressMonth.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentHub.ViewModel;
+
+namespace DocumentHub.Model
+{
+    public class WorkProgressMonth
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Month { get; set; }
+        public int WorkProgressId { get; set; }
+
+        [ForeignKey("WorkProgressId")]
+        public WorkProgress WorkProgress { get; set; }
+    }
+}
