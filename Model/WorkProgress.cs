cc52036f46707dafdcc5716efa331a2077364680
khanhdiii <khanh12369874@gmail.com>
2026-01-11 14:31:43 +0700

    fix/work-progress: fix optimize and check null

--- Model/WorkProgress.cs
+++ Model/WorkProgress.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
+using DocumentHub.Model; // để dùng Person
 
 namespace DocumentHub.ViewModel
 {
@@ -10,64 +11,51 @@ namespace DocumentHub.ViewModel
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id
-        {
-            get; set;
-        }
+        public int Id { get; set; }
 
         private string _name;
         public string Name
         {
-            get => _name; set
+            get => _name;
+            set
             {
                 _name = value;
                 OnPropertyChanged();
             }
         }
 
-        public DateTime? StartDate
-        {
-            get; set;
-        }
-        public DateTime? Deadline
-        {
-            get; set;
-        }
-
-        public class Person
-        {
-            public string FullName
-            {
-                get; set;
-            }
-        }
-        public Person Assigner
-        {
-            get; set;
-        }
-        public Person PersonInCharge
-        {
-            get; set;
-        }
-
-        public string Status
-        {
-            get; set;
-        }
-        public string Priority
-        {
-            get; set;
-        }
+        //Percen progress
+        private int _progress;
         public int Progress
         {
-            get; set;
-        }
-        public DateTime? ActualCompletionDate
-        {
-            get; set;
+            get => _progress;
+            set
+            {
+                if (_progress != value)
+                {
+                    _progress = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-        // IMPORTANT:  PropertyChanged
+        public DateTime? StartDate { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int? AssignerId { get; set; }
+
+        [ForeignKey("AssignerId")]
+        public Person Assigner { get; set; }
+
+        public int? PersonInChargeId { get; set; }
+
+        [ForeignKey("PersonInChargeId")]
+        public Person PersonInCharge { get; set; }
+
+        public string Status { get; set; }
+        public string Priority { get; set; }
+
+        public DateTime? ActualCompletionDate { get; set; }
+
         private bool _isMonth;
         public bool IsMonth
         {
@@ -90,6 +78,18 @@ namespace DocumentHub.ViewModel
             }
         }
 
+        private DateTime? _notificationDate;
+
+        public DateTime? NotificationDate
+        {
+            get => _notificationDate;
+            set
+            {
+                _notificationDate = value;
+                OnPropertyChanged();
+            }
+        }
+
         private bool _isYearly;
         public bool IsYearly
         {
@@ -101,23 +101,22 @@ namespace DocumentHub.ViewModel
             }
         }
 
-
-        private DateTime? _notificationDate;
-        public DateTime? NotificationDate
+        private int _yearlyCount;
+        public int YearlyCount
         {
-            get => _notificationDate;
+            get => _yearlyCount;
             set
             {
-                _notificationDate = value;
+                _yearlyCount = value;
                 OnPropertyChanged();
             }
         }
 
-
         private bool _is3Months;
         public bool Is3Months
         {
-            get => _is3Months; set
+            get => _is3Months;
+            set
             {
                 _is3Months = value;
                 OnPropertyChanged();
@@ -127,7 +126,8 @@ namespace DocumentHub.ViewModel
         private bool _is6Months;
         public bool Is6Months
         {
-            get => _is6Months; set
+            get => _is6Months;
+            set
             {
                 _is6Months = value;
                 OnPropertyChanged();
@@ -137,7 +137,8 @@ namespace DocumentHub.ViewModel
         private bool _is9Months;
         public bool Is9Months
         {
-            get => _is9Months; set
+            get => _is9Months;
+            set
             {
                 _is9Months = value;
                 OnPropertyChanged();
@@ -147,7 +148,8 @@ namespace DocumentHub.ViewModel
         private bool _isSudden;
         public bool IsSudden
         {
-            get => _isSudden; set
+            get => _isSudden;
+            set
             {
                 _isSudden = value;
                 OnPropertyChanged();
@@ -157,23 +159,29 @@ namespace DocumentHub.ViewModel
         private bool _isSeminar;
         public bool IsSeminar
         {
-            get => _isSeminar; set
+            get => _isSeminar;
+            set
             {
                 _isSeminar = value;
                 OnPropertyChanged();
             }
         }
 
-        public DateTime? SeminarDate
-        {
-            get; set;
-        }
-        public DateTime? SuddenDate
-        {
-            get; set;
-        }
+        [NotMapped]
+        public string SelectedMonths { get; set; }
+
+        [NotMapped]
+        public string SelectedYears { get; set; }
+
+        public DateTime? SeminarDate { get; set; }
+        public DateTime? SuddenDate { get; set; }
+
+        // Navigation properties  EF know relation ship 1-more
+        public ICollection<WorkProgressMonth> Months { get; set; } = new List<WorkProgressMonth>();
+        public ICollection<WorkProgressYear> Years { get; set; } = new List<WorkProgressYear>();
 
         public event PropertyChangedEventHandler PropertyChanged;
+
         protected void OnPropertyChanged([CallerMemberName] string n = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
     }
