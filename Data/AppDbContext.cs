9ab3c5c9da13ee2115ac230b535441b1df9dcf12
khanhdiii <khanh12369874@gmail.com>
2026-01-14 09:39:04 +0700

    feat/DataContext: add Data in Database

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,17 +7,54 @@ namespace DocumentHub.Data
 {
     internal class AppDbContext : DbContext
     {
-        public DbSet<IncomingDocument> IncomingDocuments { get; set; }
-        public DbSet<UserCredential> UserCredentials { get; set; }
-        public DbSet<OutgoingDocument> OutgoingDocuments { get; set; }
-        public DbSet<ReceivingOfficer> ReceivingOfficers { get; set; }
-        public DbSet<ConstructionStaff> ConstructionStaff { get; set; }
-        public DbSet<Signer> Signers { get; set; }
-        public DbSet<Recipient> Recipients { get; set; }
-        public DbSet<WorkProgress> WorkProgresses { get; set; }
-        public DbSet<Person> People { get; set; }
-        public DbSet<WorkProgressMonth> WorkProgressMonths { get; set; }
-        public DbSet<WorkProgressYear> WorkProgressYears { get; set; }
+        public DbSet<IncomingDocument> IncomingDocuments
+        {
+            get; set;
+        }
+        public DbSet<UserCredential> UserCredentials
+        {
+            get; set;
+        }
+        public DbSet<OutgoingDocument> OutgoingDocuments
+        {
+            get; set;
+        }
+        public DbSet<ReceivingOfficer> ReceivingOfficers
+        {
+            get; set;
+        }
+        public DbSet<ConstructionStaff> ConstructionStaff
+        {
+            get; set;
+        }
+        public DbSet<Signer> Signers
+        {
+            get; set;
+        }
+        public DbSet<Recipient> Recipients
+        {
+            get; set;
+        }
+        public DbSet<WorkProgress> WorkProgresses
+        {
+            get; set;
+        }
+        public DbSet<Person> People
+        {
+            get; set;
+        }
+        public DbSet<WorkProgressMonth> WorkProgressMonths
+        {
+            get; set;
+        }
+        public DbSet<WorkProgressQuater> WorkProgressQuaters
+        {
+            get; set;
+        }
+        public DbSet<WorkProgressYear> WorkProgressYears
+        {
+            get; set;
+        }
 
 
 
@@ -48,6 +85,11 @@ namespace DocumentHub.Data
                 .HasOne(o => o.Recipient)
                 .WithMany()
                 .HasForeignKey("RecipientId");
+
+            modelBuilder.Entity<WorkProgressQuater>()
+                .HasOne(q => q.WorkProgress)
+                .WithMany(w => w.Quarters)
+                .HasForeignKey(q => q.WorkProgressId);
 
             modelBuilder.Entity<WorkProgress>().HasOne(w => w.Assigner).WithMany().HasForeignKey(w => w.AssignerId);
             modelBuilder.Entity<WorkProgress>().HasOne(w => w.PersonInCharge).WithMany().HasForeignKey(w => w.PersonInChargeId);
